cdfecc7bb89044e34d0b4720734ffd3c94431674
garedb <garedbarney@gmail.com>
2020-02-19 10:26:43 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,26 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-const cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay === 2) {
-
-} else if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]){
-	alert("Sorry, try again.")
+let checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
 }
+
+let flipCard = function(cardId) {
+	flipCard(0); {
+	}
+	flipCard(2); {
+	}
+		if (cardsInPlay === 2) {
+	}
+
+	} else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again.")
+	}
+	console.log("User flipped queen" + cards[cardId] || "User flipped king" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
